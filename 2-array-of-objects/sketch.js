3c343775e6ff950dc076ac8164a28408605b92e2
GrahamYater <yaterg@gmail.com>
2019-10-24 15:13:29 -0700

    Made a laser and a Circle when Keypressed

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -18,24 +18,57 @@ function draw(){
 	  }
 }
 
-function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
-  let  b = new Ball(100, 100);
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){ //every time you push a key, make a new ball from the ball class and add it to the balls array
+  let  b = new Ball(100, 100,"blue");
   balls.push(b);
   console.log(balls);
+
+}
+
+else if(keyCode === RIGHT_ARROW) {
+  let  b = new laser(100, 200,"red");
+  balls.push(b);
+  console.log(balls);
+}
 }
 
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y){ //every ball needs an x value and a y value
+	constructor(x,y,color){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
+        this.color = color;
+
 	}
 
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
-    		fill("red");
+    		fill(this.color);
 		    ellipse(this.x,this.y,10,10);
+	}
+
+	moveBall(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+2;
+		this.y = this.y+.5;
+	}
+
+
+}
+
+class laser {
+
+	constructor(x,y,color){ //every ball needs an x value and a y value
+		    this.x = x;
+    		this.y = y;
+        this.color = color;
+
+	}
+
+	drawBall(){  // draw a ball on the screen at x,y
+    		fill(this.color);
+		    rect(this.x,this.y,60,1);
 	}
 
 	moveBall(){ //update the location of the ball, so it moves across the screen
