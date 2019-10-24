49e0493e14cc961200d1f2b36a5c71666bb2cadd
GrahamYater <yaterg@gmail.com>
2019-10-24 14:34:17 -0700

    Added a Cat changed Dog Colors

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,11 +1,17 @@
 //create a variable to hold one ball
 let b;
-let anotherBall;
+let anotherDog;
+let anotherDog2
+let cat
 
 function setup() {
   createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  // b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
+  // anotherBall = new Ball(200,20,"green");
+  b = new Dog(0, 100,"red",5);
+  anotherDog = new Dog(200,20,"green",30);
+  anotherDog2 = new Dog(300,30,"blue",15);
+  cat = new Cat(250,0,"yellow",30)
 }
 
 
@@ -13,24 +19,48 @@ function draw(){
 	background(220);
     b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
-    anotherBall.drawBall();
-    anotherBall.moveBall();
+    anotherDog.drawBall();
+    anotherDog.moveBall();
+    anotherDog2.drawBall();
+    anotherDog2.moveBall();
+    cat.drawBall()
+    cat.moveBall()
 
 }
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Dog {
 
-	constructor(x,y,color){ //every ball needs an x value and a y value
+	constructor(x,y,color,diameter){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
+        this.diameter = diameter;
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    ellipse(this.x,this.y,this.diameter,this.diameter);
+	}
+	moveBall(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+2;
+		this.y = this.y+.5;
+	}
+}
+
+class Cat {
+
+	constructor(x,y,color,diameter){ //every ball needs an x value and a y value
+		    this.x = x;
+    		this.y = y;
+        this.color= color;
+        this.diameter = diameter;
+	}
+	drawBall(){  // draw a ball on the screen at x,y
+    		stroke(0);
+    		fill(this.color);
+		    rect(this.x,this.y,this.diameter,this.diameter);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
