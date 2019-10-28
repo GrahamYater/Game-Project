de0a454dfa4f24307ec02ade4e24505898c4ec34
GrahamYater <yaterg@gmail.com>
2019-10-28 13:49:40 -0700

    Created Human and random Colors

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -5,22 +5,25 @@ let balls = [];
 let me;
 
 
+
 function setup() {
   createCanvas(500, 400);
+  background(random(0,255), random(0,255), random(0,255));
 
   //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
+  me = new Avatar(width/10, 300, 5);
 
 }
 
 function draw(){
-	background(220);
+
+
 
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3, random(-2,2));
+  if (frameCount % 15 == 0) {
+      let  b = new Ball(width, random(0,height), -3, random(-2,2), random(0,255), random(0,255), random(0,255), false);
       balls.push(b);
       console.log(balls); //print the balls array to the console
     }
@@ -44,9 +47,9 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
+    		stroke(random(0,255), random(0,255), random(0,255));
         strokeWeight(3);
-    		fill("blue");
+    		fill(random(0,255), random(0,255), random(0,255));
 		    ellipse(this.x,this.y,20,20);
         line(this.x,this.y, this.x, this.y+40);
         line(this.x, this.y+40, this.x-20, this.y+60);
@@ -64,6 +67,13 @@ class Avatar {
     if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
         this.y += this.speed;
     }
+    if (keyIsDown(LEFT_ARROW)) { //if you hold the up arrow, move up by speed
+       this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) { // if you hold the down arrow, move down by speed
+        this.x += this.speed;
+    }
 	}
 
   die(){
@@ -77,19 +87,23 @@ class Avatar {
 class Ball {
 
 	//every ball needs an x value, a y value, and a speed
-	constructor(x,y, speed, yspeed){
+	constructor(x,y, speed, yspeed, r, g, b, flipped){
 		this.x = x;
     this.y = y;
     this.speed = speed;
     this.yspeed = yspeed;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.flipped =flipped;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
     	stroke(0);
-      strokeWeight(1);
-    	fill("red");
-		  ellipse(this.x,this.y,10,10);
+      strokeWeight(2);
+    	fill(this.r, this.g, this.b);
+		  rect(this.x,this.y,10,20);
 	}
 
 	//update the location of the ball, so it moves across the screen
@@ -100,20 +114,25 @@ class Ball {
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
+    	 if (this.x >= me.x-75 && this.x <= me.x+75 && this.y > me.y-80 && this.y < me.y+80 && this.flipped == false){
       			this.speed = -this.speed;
+            this.flipped = true
     		}
-        else if(this.x == 3){
+        else if(this.x < 3){
           this.speed *=-1
+          this.flipped = false
         }
-        else if(this.x == 500){
+        else if(this.x > 500){
           this.speed = -this.speed
+          this.flipped = false
         }
-        else if(this.y == 0){
+        else if(this.y < 0){
           this.yspeed = -this.yspeed
+          this.flipped = false
         }
-        else if(this.y == 400){
+        else if(this.y > 380){
           this.yspeed = -this.yspeed
+          this.flipped = false
         }
   	}
 
